e171e6460f0d52428f1279df02c26ab8ee861f9b
Maximiliano Arias <maxim_arias_iliano@yahoo.com.ar>
2018-12-10 23:10:26 -0300

    Agrego llamada a api

--- TiendaVirtualWeb/app.js
+++ TiendaVirtualWeb/app.js
@@ -1,34 +1,47 @@
 ﻿(function () {
     var app = angular.module('productos', []);
 
-    app.controller('ProductosController', function () {
-        this.productos = prods;
+    app.controller('ProductosController', function ($scope, $http) {
+        $scope.productos = prods;
+        $http.get("http://35.185.15.6/WebApi/api/Producto").then(function successCallback(response) {
+            alert("success");
+        }, function errorCallback(response) {
+            alert(response.status);
+        });
     });
 
     var prods = [
         {
-            id: 1,
-            nombre: 'Computadora',
-            descripcion: 'Intel i5',
-            precio: 295,
+            "IdProducto": 1,
+            "Nombre": "Laptop HP",
+            "Descripcion": "Laptop HP PAVILION",
+            "Foto": "foto1.jpg",
+            "Precio": 27500,
+            "Stock": 250,
+            "FechaAlta": "0001-01-01T00:00:00",
+            "Habilitado": true,
+            "IdCategoria": 10
         },
         {
-            id: 2,
-            nombre: 'Tarjeta de video',
-            descripcion: 'Nvidia geforce',
-            precio: 50,
+            "IdProducto": 5,
+            "Nombre": "Mochila",
+            "Descripcion": "Mochila negra para laptop hasta 17\"",
+            "Foto": "mochila1.jpg",
+            "Precio": 899,
+            "Stock": 350,
+            "FechaAlta": "2018-12-09T00:00:00",
+            "Habilitado": true,
+            "IdCategoria": 13
         },
         {
-            id: 3,
-            nombre: 'Impresora 3d',
-            descripcion: 'La mejor del mercado, descripcion alargada a proposito para chequear visualizacion, longitud de 150 caracteres que es el maximo que soporta la base de',
-            precio: 500,
-        },
-        {
-            id: 4,
-            nombre: 'Servidor',
-            descripcion: 'MegaServer 4000',
-            precio: 2000,
-        }
-    ];
+            "IdProducto": 6,
+            "Nombre": "PC",
+            "Descripcion": "Computadora Personal incluye monitor,teclado y mouse",
+            "Foto": "PC.jpg",
+            "Precio": 28000,
+            "Stock": 700,
+            "FechaAlta": "2018-12-09T00:00:00",
+            "Habilitado": true,
+            "IdCategoria": 12
+        }];
 })();
