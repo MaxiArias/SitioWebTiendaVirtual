5d01cb80521de78f94c61f5e4fcc6ed30915bfcf
Cecilia Martínez <cecilia.martinezcurbelo@hotmail.com>
2018-12-13 21:59:49 -0300

    CM 13/12/2018-3

--- TiendaVirtualWeb/app.js
+++ TiendaVirtualWeb/app.js
@@ -3,12 +3,14 @@
 
     app.controller("ProductosController", function ($scope, $http) {
       
-        $http.get("http://localhost:49559/api/producto").then(function successCallback(response) {
+        $http.get("http://35.185.15.6/WebApi/api/producto").then(function successCallback(response) {
             $scope.productos1 = response.data;
-            alert("success");
         }, function errorCallback(response) {
-            alert(response.status);
         });
+    });
+
+    app.controller("LoginController", function ($scope) {
+
     });
 
 })();
