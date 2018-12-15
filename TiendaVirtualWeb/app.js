780436901da0f1be0c7bc5a54170b9f0ee630d8e
Maximiliano Arias <maxim_arias_iliano@yahoo.com.ar>
2018-12-15 12:11:24 -0300

    Se agrega funcion login en app.js

--- TiendaVirtualWeb/app.js
+++ TiendaVirtualWeb/app.js
@@ -3,14 +3,31 @@
 
     app.controller("ProductosController", function ($scope, $http) {
       
-        $http.get("http://35.185.15.6/WebApi/api/producto").then(function successCallback(response) {
+        $http.get("http://35.185.15.6/WebApi/api/Producto").then(function successCallback(response) {
             $scope.productos1 = response.data;
         }, function errorCallback(response) {
+            alert("Ha ocurrido un error");
         });
     });
 
-    app.controller("LoginController", function ($scope) {
-
+    app.controller("LoginController", function ($scope, $http) {
+        $scope.Login = function () {
+            var parameter = JSON.stringify({ mail: "juan@prueba.com", password: "123456" });
+            $http.post("http://35.185.15.6/WebApi/api/Usuario/Login", parameter).then(function successCallback(response) {
+                alert(response.data);
+            }, function errorCallback(response) {
+                alert(response.statusText);
+            });
+        }
     });
 
 })();
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
