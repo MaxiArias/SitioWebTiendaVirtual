b8564678214fee24bc6108a960ceb983253fadc1
Cecilia Martínez <cecilia.martinezcurbelo@hotmail.com>
2018-12-16 11:09:40 -0300

    CM 16/12-1

--- TiendaVirtualWeb/app.js
+++ TiendaVirtualWeb/app.js
@@ -22,13 +22,29 @@
         }
     });
 
+    app.controller("ClienteController", function ($scope, $rootScope, $http) {
+        $scope.Login = function () {
+            var parameter = JSON.stringify({ mail: $scope.mail, password: $scope.password });
+            $http.post("http://35.185.15.6/WebApi/api/Cliente", parameter).then(function successCallback(response) {
+                $rootScope.id = response.data;
+                modal.style.display = 'none';
+            }, function errorCallback(response) {
+                alert(response.statusText);
+            });
+        }
+    });
+
 })();
 
 var modal = document.getElementById('id01');
+var modal1 = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+    if (event.target == modal1) {
+        modal1.style.display = "none";
     }
 }
